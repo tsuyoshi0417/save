3b2b4569a09e81d0bdebe0a734811ad192e49203
Your Name <your.email@example.com>
2025-12-09 17:14:05 +0900

    更新

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
-//1.Html要素を取ってくる
-const message = document.getElementById("message")
-const button = document.getElementById("changeMessageButton")
+//1.HTML要素を取得
+const message = document.getElementById('message');
+const button = document.getElementById("changeMessageButton");
 
-//2最初の文字
-message.textContent = "javaScript準備出来ました";
+//2.メッセージ一覧を配列で管理
+const messages = [
+    "Javasctript準備出来ました!",
+    "ボタンが押されました!🎉",
+    "いい感じです",
+    "何度でも変わるよ",
+];
 
-//ボタン押された時の動き
-button.addEventListener("click",()=>{
-message.textContent = "ボタンが押されました！🎉"
+//3.今どれを表示しているか（最初は０）
+let index = 0;
+message.textContent = messages[index];
+
+//4.ボタンが押された時の動き
+button.addEventListener("click", () => {
+    //インデックスを1つ進める
+    index++;
+    //配列の長さを超えたら0に戻す
+    if (index >= messages.length) {
+        index = 0;
+    }
+    //メッセージを更新
+    message.textContent = messages[index];
 });
