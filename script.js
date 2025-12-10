70085d2f5a2dce1d097cda122d8d15495a44eb65
Your Name <your.email@example.com>
2025-12-10 10:18:47 +0900

    test4

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
 //1.HTML要素を取得
-const message = document.getElementById('message');
-
-// 危険；nullのtextcontentを書き換える
-message.textContent ="こんにちは！";
-
+const messageElement = document.getElementById('message');
 const button = document.getElementById("changeMessageButton");
 
 //2.メッセージ一覧を配列で管理
-const message = [
+const messages = [
     "Javascript準備出来ました!",
     "少しずつ成長中...",
     "コードで世界を広げて行く",
@@ -16,6 +12,27 @@ const message = [
 
 //3.今どれを表示しているか（最初は０）
 let index = 0;
+
+//*Aパターン;null チェックを入れた安全な書き方
+if (messageElement) {
+   //最初のメッセージ
+    messageElement.textContent = messages[index];
+
+    //4.ボタンが押された時の動き
+    button.addEventListener("click", () => {
+        //インデックスを1つ進める
+        index++;
+        //配列の長さを超えたら0に戻す
+        if (index >= messages.length) {
+            index = 0;
+        }
+        //メッセージを更新
+        messageElement.textContent = messages[index];
+    });
+
+}
+
+
 message.textContent = message[index];
 
 //4.ボタンが押された時の動き
