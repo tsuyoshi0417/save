60529f5ec680c052b1f7c1d481a46bc6fe48496c
Your Name <your.email@example.com>
2025-12-12 09:26:07 +0900

    kousin

--- slider/slider.js
+++ slider/slider.js
@@ -4,7 +4,7 @@ const slideImage = document.getElementById('slideImage');
 const nextbutton = document.getElementById('nextButton');
 
 const images = [
-    'images/3089298F-9C80-4B91-8BBE-C2EAACF18F44_1_100_o.jpeg',
+    'images/image1.jpeg',
     'images/photo2.jpg',
     'images/photo3.jpg',
 ];
