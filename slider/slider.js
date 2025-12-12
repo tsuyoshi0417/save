f0022dd4f2d9b283feccd085cd20aeee7e89afd2
Your Name <your.email@example.com>
2025-12-12 09:35:08 +0900

    hennkou

--- slider/slider.js
+++ slider/slider.js
@@ -1,17 +1,24 @@
 let index = 0;
 
+// HTML から要素を取ってくる
 const slideImage = document.getElementById('slideImage');
-const nextbutton = document.getElementById('nextButton');
+const nextButton = document.getElementById('nextButton');
+const prevButton = document.getElementById('prevButton');
 
+// ここは実際のファイル名に合わせる
 const images = [
     'images/image1.jpeg',
     'images/image2.jpeg',
     'images/image3.jpeg',
 ];
-nextbutton.addEventListener('click', () => {
-    index(index+1) % images.length;
-    slideImage.src = images[index ]
+
+// 「次へ」ボタン
+nextButton.addEventListener('click', () => {
+    // index+1; だと値が変わらないので…
+    index = (index + 1) % images.length; // 0→1→2→0→…
+    slideImage.src = images[index];
 });
+
 // 「戻る」ボタン
 prevButton.addEventListener('click', () => {
     index = (index - 1 + images.length) % images.length;
